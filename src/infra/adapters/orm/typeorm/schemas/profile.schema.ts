c3649c398e9023540ec84b7a4931acb2c6de2192
Jaymeson Mendes <jaymesonmendes@gmail.com>
2024-11-13 19:12:26 -0300

    feat: schema

---
+++ src/infra/adapters/orm/typeorm/schemas/profile.schema.ts
@@ -0,0 +1,53 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { ProfileType } from "../../../../../shared/enums/profile-type.enum";
+
+@Entity()
+export class Profile {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({
+    type: "enum",
+    enum: ProfileType,
+  })
+  type: ProfileType;
+
+  @Column({ nullable: true })
+  cnpj: string;
+
+  @Column({ nullable: false })
+  cpf: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  cell: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  zipCode: string;
+
+  @Column()
+  street: string;
+
+  @Column()
+  number: string;
+
+  @Column({ nullable: true })
+  addition: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  neighborhood: string;
+
+  @Column()
+  state: string;
+}
