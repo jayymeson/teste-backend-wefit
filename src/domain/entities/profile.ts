0ccec561ee11cb25e4f610aa47d5cdee1e3e8b62
Jaymeson Mendes <jaymesonmendes@gmail.com>
2024-11-13 19:19:05 -0300

    feat: class implements create profile interface

---
+++ src/domain/entities/profile.ts
@@ -0,0 +1,23 @@
+import { CreateProfileInterface } from "../contracts/create-profile.interface";
+import { ProfileType } from "../../shared/enums/profile-type.enum";
+
+export class Profile implements CreateProfileInterface {
+  constructor(
+    public type: ProfileType,
+    public cnpj: string = "", 
+    public cpf: string = "",  
+    public name: string,
+    public cell: string,
+    public phone: string,
+    public email: string,
+    public address: {
+      zipCode: string;
+      street: string;
+      number: string;
+      addition?: string;
+      city: string;
+      neighborhood: string;
+      state: string;
+    }
+  ) {}
+}
